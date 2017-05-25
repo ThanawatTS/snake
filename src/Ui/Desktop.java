8bdb00adf9d75095dbc9b9ceee57000b24b4137e
GamTS <Spirit_windwalk@hotmail.com>
2017-05-25 14:15:56 +0700

    add keylis

--- src/Ui/Desktop.java
+++ src/Ui/Desktop.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -15,7 +17,7 @@ import javax.swing.JPanel;
 import Game.Game;
 
 
-public class Desktop extends JFrame {
+public class Desktop extends JFrame implements KeyListener{
 
 	private JPanel main;
 	private JButton replaybtn;
@@ -45,14 +47,28 @@ public class Desktop extends JFrame {
 			}
 		};
 		
-		
 		main.setPreferredSize(new Dimension(Width,Height));
 		main.setDoubleBuffered(true);
 		add(main);
 		
 		replaybtn = new JButton("Replay");
-		replaybtn.setEnabled(false);
+		replaybtn.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.print("asas");
+			}
+			
+		});
+		replaybtn.setEnabled(true);
 		add(replaybtn, BorderLayout.SOUTH);
+	}
+	
+	private void initControl(){
+		this.addKeyListener(this);
+		main.addKeyListener(this);
+		replaybtn.addKeyListener(this);
 	}
 	
 	private void draw(Graphics g){
@@ -81,12 +97,29 @@ public class Desktop extends JFrame {
 		
 	}
 	
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	public static void main(String[] args){
 		Desktop desktop = new Desktop();
 		desktop.setVisible(true);
 		desktop.start();
 	}
-	
-	
 	
 }
